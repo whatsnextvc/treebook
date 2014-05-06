d34c58965f67b3eecacd3a58e5e677471b4c4789
whatsnextvc <rishireddy09@gmail.com>
2014-05-06 12:19:32 -0500

    Included Profile Correctness Tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter a first name" do
+  	user=User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+end
+
+class UserTest < ActiveSupport::TestCase
+  test "a user should enter a last name" do
+  	user=User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+end
+
+
+class UserTest < ActiveSupport::TestCase
+  test "a user should enter a profile name" do
+  	user=User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+end
+
+test "a user should enter a unique profile name" do
+    user = User.new
+    user.profile_name = users(:roberto).profile_name
+    assert !user.save
+    #puts user.errors.inspect
+    assert !user.errors[:profile_name].empty?
+  end   
+
+end
+end
 end
